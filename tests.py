3bfea42dcb982c2fde5c78f39ac8d9f6c91c7ad0
Aaron O'Leary <aaron.oleary@gmail.com>
2013-08-02 21:51:38 +0100

    comparison of scipy / my cwt

--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
 from nose.tools import *
+import numpy.testing as npt
 
 import numpy as np
+import scipy.signal
 
+import wavelets
 from wavelets import Wavelets
 from wavelets import WaveletAnalysis
 
@@ -13,3 +16,21 @@ wa = WaveletAnalysis(x)
 
 def test_N():
     assert_equal(N, wa.N)
+
+def test_compare_cwt():
+    """Compare the output of Scipy's cwt (using direct convolution)
+    and my cwt (using fft convolution).
+    """
+    cwt = scipy.signal.cwt
+    fft_cwt = wavelets.fft_cwt
+
+    data = np.random.random(2000)
+    wave_anal = WaveletAnalysis(data, wavelet='ricker')
+    widths = wave_anal.scales[::-1]
+
+    morlet = scipy.signal.morlet
+
+    cwt = cwt(data, morlet, widths)
+    fft_cwt = fft_cwt(data, morlet, widths)
+
+    npt.assert_array_almost_equal(cwt, fft_cwt, decimal=13)
