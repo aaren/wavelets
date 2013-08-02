5c7151d2d8018ddcec0cbd396bf1ffd1e14eadb8
Aaron O'Leary <aaron.oleary@gmail.com>
2013-08-02 17:11:34 +0100

    wavelet function container, base analysis class

---
+++ wavelets.py
@@ -0,0 +1,30 @@
+from functools import wraps
+
+import numpy as np
+import scipy.signal
+
+
+class Wavelets(object):
+    """Container for various wavelet basis functions.
+
+    To be admissible as a wavelet, a function must:
+
+    - have zero mean
+    - be localised in both time and frequency space
+
+    These functions are a function of a dimensionless time
+    parameter.
+    """
+    # morlet wavelet
+    morlet = scipy.signal.morlet
+    # ricker wavelet
+    ricker = scipy.signal.ricker
+    # aka Derivitive Of Gaussian order 2, mexican hat or marr
+    dog2 = ricker
+
+
+class WaveletAnalysis(object):
+    def __init__(self, x):
+        """Arguments:
+            x - 1 dimensional input signal
+        """
