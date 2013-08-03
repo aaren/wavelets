6593e023e0182267e425b37586f47c2221ee41ef
Aaron O'Leary <aaron.oleary@gmail.com>
2013-08-03 18:42:04 +0100

    copy morlet from scipy

--- wavelets.py
+++ wavelets.py
@@ -49,10 +49,63 @@ def fft_cwt(data, wavelet, widths):
                                                             mode='same')
     return output
 
-@wraps(scipy.signal.morlet)
-def morlet(M, s=1.0, w0=5.0, complete=False):
-    """Wrap scipy morlet function with arguments in the right order. """
-    return scipy.signal.morlet(M, w0, s, complete)
+def morlet(M, w=5.0, s=1.0, complete=True):
+    """
+    Complex Morlet wavelet.
+
+    Parameters
+    ----------
+    M : int
+        Length of the wavelet.
+    w : float
+        Omega0. Default is 5
+    s : float
+        Scaling factor, windowed from ``-s*2*pi`` to ``+s*2*pi``. Default is 1.
+    complete : bool
+        Whether to use the complete or the standard version.
+
+    Returns
+    -------
+    morlet : (M,) ndarray
+
+    See Also
+    --------
+    scipy.signal.gausspulse
+
+    Notes
+    -----
+    The standard version::
+
+        pi**-0.25 * exp(1j*w*x) * exp(-0.5*(x**2))
+
+    This commonly used wavelet is often referred to simply as the
+    Morlet wavelet.  Note that this simplified version can cause
+    admissibility problems at low values of w.
+
+    The complete version::
+
+        pi**-0.25 * (exp(1j*w*x) - exp(-0.5*(w**2))) * exp(-0.5*(x**2))
+
+    The complete version of the Morlet wavelet, with a correction
+    term to improve admissibility. For w greater than 5, the
+    correction term is negligible.
+
+    Note that the energy of the return wavelet is not normalised
+    according to s.
+
+    The fundamental frequency of this wavelet in Hz is given
+    by ``f = 2*s*w*r / M`` where r is the sampling rate.
+
+    """
+    x = linspace(-s * 2 * pi, s * 2 * pi, M)
+    output = exp(1j * w * x)
+
+    if complete:
+        output -= exp(-0.5 * (w**2))
+
+    output *= exp(-0.5 * (x**2)) * pi**(-0.25)
+
+    return output
 
 class Wavelets(object):
     """Container for various wavelet basis functions.
