2acabb0ea6ba56666f84147afcfdbadd8914eb4f
Aaron O'Leary <aaron.oleary@gmail.com>
2013-08-04 18:48:19 +0100

    test wavelet reconstruction

--- tests.py
+++ tests.py
@@ -66,3 +66,29 @@ def compare_morlet(N=2000):
     fig.tight_layout()
 
     return fig
+
+
+def test_Cd():
+    """default wavelet is morlet. Terrence and Compo calculate C_d
+    for this of 0.776."""
+    assert_almost_equal(wa.C_d, 0.776, places=2)
+
+
+def test_var():
+    """The wavelet transform conserves total energy, i.e. variance.
+
+    The variance of the data should be the same as the variance of
+    the wavelet.
+
+    Check that they are within 10%
+    """
+    assert_almost_equal(wa.data_variance, wa.data_variance, places=1)
+
+
+def test_reconstruction():
+    """In principle one can reconstruct the input data from the
+    wavelet transform.
+
+    Check within 10%.
+    """
+    npt.assert_array_almost_equal(wa.data, wa.reconstruction, decimal=1)
